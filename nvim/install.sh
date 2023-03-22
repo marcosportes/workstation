0213f4e42d0bf85efe06f383765967b608adae9e
marcosportes <marcosportesjr@gmail.com>
2023-03-22 10:47:14 -0300

    Up: Nvim install.sh with appimage

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,22 +1,26 @@
 #! /bin/bash
 
 echo "
-Installing Neovim (Latest version):
+Installing AppImage Neovim (Latest version):
 "
 echo "Download/Install:"
-wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
-sudo dpkg -i nvim-linux64.deb
-rm nvim-linux64.deb
+   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+   chmod u+x nvim.appimage
+   ./nvim.appimage --appimage-extract
+   ./squashfs-root/AppRun --version
+   sudo mv squashfs-root /
+   sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+echo "Done"
 
-mkdir ~/.config/nvim
-mkdir ~/.config/lua
 
 echo "Installing vim-plug"
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
+   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 echo "Done!"
 
-# restore backed up files by creating a hard link
 
 # After downloading and giving an error in the coc, run in ~/.config/nvim/plugged: $ yarn install 
+
+
+
+
